efb56485046d00b9b24fe42956d6209963cfe60e
Berita-Winnie <beritawinnie@gmail.com>
2025-11-09 06:20:14 +0300

    initial set up complete

---
+++ src/assets/assets.js
@@ -0,0 +1,7 @@
+import dinner from './dinner.plugin'
+import hero from './hero.png'
+import humburgur from './humburgur.png'
+import lunch from './lunch.png'
+import morning from './morning.png'
+
+export const images = { dinner, hero, humburgur, lunch, morning }
